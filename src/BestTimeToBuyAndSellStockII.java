9e11024c1987a38736eaa2f47c2e0af2e9a893a5
vult137 <wangsongyi08@163.com>
2020-08-06 11:00:53 -0400

    90. Subsets II  122. Best Time to Buy and Sell Stock II

---
+++ src/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,10 @@
+// 122. Best Time to Buy and Sell Stock II
+public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++)
+            if (prices[i] < prices[i + 1])
+                profit += prices[i + 1] - prices[i];
+        return profit;
+    }
+}
