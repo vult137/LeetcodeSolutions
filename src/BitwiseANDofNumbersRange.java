bcb4bcdceae2277dec90b967841827c400216e07
vult137 <wangsongyi08@163.com>
2021-04-14 16:42:43 +0800

    201. Bitwise AND of Numbers Range

--- src/BitwiseANDofNumbersRange.java
+++ src/BitwiseANDofNumbersRange.java
@@ -1,11 +1,16 @@
 // 201. Bitwise AND of Numbers Range
 public class BitwiseANDofNumbersRange {
-    public int rangeBitwiseAnd(int left, int right) {
-        int res = left;
-        for (int i = left + 1; i <= right; i++) {
-            res = res & i;
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0){
+            return 0;
         }
-        return res;
+        int moveFactor = 1;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            moveFactor <<= 1;
+        }
+        return m * moveFactor;
     }
 
     public static void main(String[] args) {
